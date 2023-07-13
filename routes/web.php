9ae534a7f3a861f0171d641831606c8075bea030
brayan <brayan6omez@gmail.com>
2023-07-13 09:05:55 -0500

    login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UsuarioController;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +11,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('home', UsuarioController::class);
+

8d43e54a9fec4b19901f5864915f9dbb4d2e6dfc
brayan <brayan6omez@gmail.com>
2023-07-13 09:22:49 -0500

    segunda rama

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\UsuarioController;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-
 Route::get('/', function () {
     return view('welcome');
 });
